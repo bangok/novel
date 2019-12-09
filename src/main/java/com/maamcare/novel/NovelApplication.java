1edecb1fa40ead5f60cf8c1462d10e06fcf4cf12
zcf <2764300331@qq.com>
2019-12-09 15:56:27 +0800

    添加控制台输入

--- src/main/java/com/maamcare/novel/NovelApplication.java
+++ src/main/java/com/maamcare/novel/NovelApplication.java
@@ -1,6 +1,8 @@
 package com.maamcare.novel;
 
 
+import java.util.Scanner;
+
 public class NovelApplication {
 
     public static String baseUrl = "https://www.biquguan.com/bqg403614/";
@@ -11,6 +13,20 @@ public class NovelApplication {
 
 
     public static void main(String[] args) {
+        //提示输入
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("爬取网站地址为：https://www.biquguan.com/");
+        System.out.println("请输入要爬取小说的头地址，示例：https://www.biquguan.com/bqg403614/");
+        String myBaseUrl = scanner.nextLine();
+        baseUrl = myBaseUrl;
+        System.out.println("请输入要爬取小说的第一张地址，示例：2274516.html");
+        String myFirstChapterUrl = scanner.nextLine();
+        firstChapterUrl = myFirstChapterUrl;
+        System.out.println("请输入要存到本机的文件地址，示例：C:\\少女骑士零.txt");
+        String myFileUrl = scanner.nextLine();
+        fileUrl = myFileUrl;
+
         System.out.println("爬虫开始");
         while (true){
             String nextChapterUrl = NovelUtil.getOneChapter(baseUrl,fileUrl,firstChapterUrl);
